ea6b902fdcc64fce7c231ee7ce6db96280ddd6c0
FahmiSyahrulah <fahmi@alphatech.id>
2019-03-20 17:30:33 +0700

    edit

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ cd /home/ubuntu/commerce &&
 git pull
 
 source ~/.profile
-echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
+echo "$DOCKERHUB_PASS" | sudo docker login --username $DOCKERHUB_USER --password-stdin
 sudo docker stop commerce
 sudo docker rm commerce
 sudo docker rmi fsyahrulah/commerce
