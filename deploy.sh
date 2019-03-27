9eecded9dfe45453a1d37c14a6eb411f44ae4011
FahmiSyahrulah <fahmi@alphatech.id>
2019-03-27 10:59:23 +0700

    edited

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ echo "$DOCKERHUB_PASS" | sudo docker login --username $DOCKERHUB_USER --password
 sudo docker stop commerce
 sudo docker rm commerce
 sudo docker rmi fsyahrulah/commerce
-sudo docker run -d --name commerce -p 5000:5000 fsyahrulah/commerce:latest
+sudo docker run -d --name commerce -p 5000:5000 fsyahrulah/commerce:development
